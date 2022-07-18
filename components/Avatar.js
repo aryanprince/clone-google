f11e221ca5232df43e20148b67495d3a281af30c
aryanprince <aryanprincesanty@icloud.com>
2022-07-18 11:15:32 +0530

    2:13:48 Search page UI implemented

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
-function Avatar({ url }) {
-    return <img src={url} className="h-10 rounded-full cursor-pointer transition duration-150  transform hover:scale-110 " alt="profile pic" loading="lazy" />
+function Avatar({ url, className }) {
+    return (
+        <img
+            src={url}
+            className={`h-10 rounded-full cursor-pointer transition duration-150  transform hover:scale-110 ${className}`}
+            alt="profile pic"
+            loading="lazy"
+        />
+    )
 }
 
 export default Avatar
