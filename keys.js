4e62f28bdf60d2b4defab77db9e49120a58da590
aryanprince <aryanprincesanty@icloud.com>
2022-07-18 14:45:44 +0530

    Updated API keys

--- keys.js
+++ keys.js
@@ -1,5 +1,7 @@
 // API Key - https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = 'AIzaSyCp5c4ZrVshZ8fJ5WXnS3oBKPnNOpEsl7I'
+export const API_KEY = 'AIzaSyD1KFGEDyX2gvyJhYSPBDNfWrnw3WsgnPc'
+// New one testing: AIzaSyD1KFGEDyX2gvyJhYSPBDNfWrnw3WsgnPc
+// Old first one: AIzaSyCp5c4ZrVshZ8fJ5WXnS3oBKPnNOpEsl7I
 
 // Context Key - https://cse.google.com/cse/create/new
 export const CONTEXT_KEY = 'f3365f14053d4de89'
