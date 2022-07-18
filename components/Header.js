ed1132d87d8f38fc1d866b77f2224d67db5f20cd
aryanprince <aryanprincesanty@icloud.com>
2022-07-18 18:25:32 +0530

    Updated avatars

--- components/Header.js
+++ components/Header.js
@@ -52,7 +52,7 @@ function Header() {
                 <img
                     loading="lazy"
                     className="h-10 rounded-full cursor-pointer transition duration-150  transform hover:scale-110 ml-auto"
-                    src="https://coaching.papareact.com/ai9"
+                    src="https://avatars.githubusercontent.com/u/45622345?v=4"
                     alt="profile pic"
                 />
             </div>
--- pages/index.js
+++ pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
                     <img
                         loading="lazy"
                         className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110"
-                        src="https://coaching.papareact.com/ai9"
+                        src="https://avatars.githubusercontent.com/u/45622345?v=4"
                         alt="profile pic"
                     />
                 </div>
