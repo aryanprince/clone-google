7a58b7d5761ef983cd2f52fe8ee8aa59390066b8
aryanprince <aryanprincesanty@icloud.com>
2022-07-18 17:54:51 +0530

    Updated keys.js

--- keys.js
+++ keys.js
@@ -1,18 +1,3 @@
-// // API Key - https://developers.google.com/custom-search/v1/using_rest
-// export const API_KEY = 'AIzaSyCp5c4ZrVshZ8fJ5WXnS3oBKPnNOpEsl7I'
-
-// // Context Key - https://cse.google.com/cse/create/new
-// export const CONTEXT_KEY = '94157dd9024564ef2'
-
-//WORKING KEYS:
-// context = 5dfda8bc663d663e4
-// api key = AIzaSyDLxcoPiQFMr3qSa-Vn7W7c3NqXqdxA5Bs
-
-// ARCHIVE KEYS:
-// context
-// NEW UPDATED 5dfda8bc663d663e4
-// Old first one: f3365f14053d4de89
-// api
-// New one testing: AIzaSyD1KFGEDyX2gvyJhYSPBDNfWrnw3WsgnPc
-// Old first one: AIzaSyCp5c4ZrVshZ8fJ5WXnS3oBKPnNOpEsl7I
-// aryyxn's key: AIzaSyDLxcoPiQFMr3qSa-Vn7W7c3NqXqdxA5Bs
+// Keys required for the application to function:
+// 1. API Key - https://developers.google.com/custom-search/v1/using_rest
+// 2. Context Key - https://cse.google.com/cse/create/new
